b0e02bec51acc087476638eb15f2146971971bf6
Felipe00726 <felipe21palacio@gmail.com>
2024-08-12 18:23:47 -0500

    carrito reponsive

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use('/api/v1', rutas)
 
 
 app.listen(3000, () => {
-    console.log('el servidor se esta ejecutando en http://localhost :3000');
+    console.log('el servidor se esta ejecutando en http://localhost:3000');
 });
